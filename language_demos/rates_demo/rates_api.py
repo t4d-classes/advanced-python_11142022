972ceb79bff578da67133709c8cbc5d2d4adb483
Eric Greene <eric@t4d.io>
2022-11-15 13:19:52 -0500

    add url comment to rates api

--- language_demos/rates_demo/rates_api.py
+++ language_demos/rates_demo/rates_api.py
@@ -16,6 +16,18 @@ def check() -> Response:
     return "READY"
 
 
+# URL: http://127.0.0.1:5050/api/2021-04-08?base=INR&symbols=USD,EUR
+# {
+#     "date": "2021-04-08",
+#     "base": "INR",
+#     "rates": {
+#         "USD": 70,
+#         "EUR": 80,
+#     }
+# }
+    
+
+
 def start_rates_api() -> None:
     """ start rates api """
 

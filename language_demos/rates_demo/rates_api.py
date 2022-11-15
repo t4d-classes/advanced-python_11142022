63237493639e8606bad672c1584e16a2989ff152
Eric Greene <eric@t4d.io>
2022-11-15 13:56:49 -0500

    final code for rates api

--- language_demos/rates_demo/rates_api.py
+++ language_demos/rates_demo/rates_api.py
@@ -6,13 +6,15 @@ import pathlib
 import time
 from flask import Flask, Response, request, jsonify, abort
 
+from rates_demo.rates_data import load_rates_from_history
+
 rates: list[dict[str,Any]] = []
 
 app = Flask(__name__)
 
 @app.route("/check") 
 def check() -> Response:
-    """ health check envdpoint """
+    """ health check endpoint """
     return "READY"
 
 
@@ -25,11 +27,49 @@ def check() -> Response:
 #         "EUR": 80,
 #     }
 # }
-    
+
+@app.route("/api/<rate_date>") 
+def rates_by_date(rate_date: str) -> Response:
+    """ rates by date endpoint """
+
+    time.sleep(0.1)
+
+    for rate in rates:
+
+        if rate["Date"] == rate_date:
+
+            base_country = request.args.get("base", "EUR")
+
+            if "symbols" in request.args:
+                country_symbols = request.args["symbols"].split(",")
+            else:
+                country_symbols = [ col for col in rate if col != "Date" ]
+
+            country_rates = {
+                country_code: country_rate / rate[base_country]
+                for (country_code, country_rate) in rate.items()
+                if country_code != "Date" and
+                country_code in country_symbols and
+                not math.isnan(country_rate)
+            }
+
+            return jsonify({
+                "date": rate["Date"],
+                "base": base_country,
+                "rates": country_rates
+            })
+
+    abort(404)
 
 
 def start_rates_api() -> None:
     """ start rates api """
+
+    global rates
+
+    rates_file_path = pathlib.Path("..", "data", "eurofxref-hist.csv")
+
+    rates = load_rates_from_history(rates_file_path)
 
     app.run(port=5050)
 

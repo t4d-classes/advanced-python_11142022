d803d972e530a2006d6f8ae7471bca1be2db35a5
Eric Greene <eric@t4d.io>
2022-11-16 12:59:16 -0500

    add solution to rates app exercise 1

--- language_demos/rates_app/rates_server.py
+++ language_demos/rates_app/rates_server.py
@@ -17,8 +17,8 @@ def command_start_server(server_process: Optional[mp.Process]) -> mp.Process:
     if server_process and server_process.is_alive():
         print("server is already running")
     else:
-        # step 1 - create a new process object 
-        # step 2 - start the new process object
+        server_process = mp.Process(target=rate_server)
+        server_process.start()
         print("server started")
 
     return server_process
@@ -37,7 +37,27 @@ def command_stop_server(
     server_process = None
 
     return server_process
-    
+
+
+def command_status(
+    server_process: Optional[mp.Process]) -> None:
+    """ command status """
+
+    if server_process and server_process.is_alive():
+        print("server is running")
+    else:
+        print("server is stopped")
+
+
+def command_exit(
+    server_process: Optional[mp.Process]) -> None:
+    """ command exit """
+
+    if server_process and server_process.is_alive():
+        server_process.terminate()
+
+    server_process = None
+
 
 def main() -> None:
     """Main Function"""
@@ -54,22 +74,18 @@ def main() -> None:
                 server_process = command_start_server(server_process)
             elif command == "stop":
                 server_process = command_stop_server(server_process)
-            # step 3 - add a command named "status" that outputs to the
-            # console if the server is current running or not
-            # hint: follow the command function pattern used by the other
-            # commands
+            elif command == "status":
+                command_status(server_process)
             elif command == "exit":
-                # step 4 - terminate the "server_process" if the
-                # "server_process" is an object and is alive
+                command_exit(server_process)
                 break
 
     except KeyboardInterrupt:
-        # step 5 - terminate the "server_process" if the
-        # "server_process" is an object and is alive
-        pass
+        command_exit(server_process)
 
     sys.exit(0)
 
 
 if __name__ == '__main__':
     main()
+    
